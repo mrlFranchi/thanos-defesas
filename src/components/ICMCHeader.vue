617164ecf4b23ac9478ff99c81d72c4b4931e532
mRlFranchi <murilo.franchi@usp.br>
2023-08-31 19:21:18 -0300

    redone header with two app-bars

--- src/components/ICMCHeader.vue
+++ src/components/ICMCHeader.vue
@@ -1,19 +1,43 @@
 <template>
-  <v-app-bar app
-    color="cyan">
-    <img
-      src="./..\assets\logo.svg"
-      width="128"
-      height="64"
-      alt="Logo do ICMC">
-    <v-spacer></v-spacer>
-    <v-tabs>
-      <v-tab to="/">
+  <div>
+    <v-app-bar
+      color="primary"
+      height="110">
+      <v-spacer></v-spacer>
+      <img
+        src="./..\assets\logo.svg"
+        width="200"
+        height="100"
+        alt="Logo do ICMC">
+        <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-app-bar
+      height="50">
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="grey darken-1"
+      >
+        <v-tab to="/"
+        >
         Home
-      </v-tab>
-      <v-tab to="/about">
-        About us
-      </v-tab>
-    </v-tabs>
-  </v-app-bar>
+        </v-tab>
+        <v-tab to="/AboutUs"
+        >
+        About Us
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      'Home',
+      'About',
+    ],
+  }),
+};
+</script>
