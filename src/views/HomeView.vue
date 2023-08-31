7e62d2692b91480cc7a81a24a84c203a509160f4
mRlFranchi <murilo.franchi@usp.br>
2023-08-31 19:39:04 -0300

    about us page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,25 @@
 <template>
   <v-main style="padding: 0;">
-  <v-toolbar>
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>Application</v-toolbar-title>
-  </v-toolbar>
-  <v-navigation-drawer
-    v-model="drawer"
-  >
-    <!--  -->
-  </v-navigation-drawer>
-    <!--  -->
+    <v-toolbar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-navigation-drawer
+      v-model="drawer"
+    >
+    <v-list-group
+      :value="true"
+      prepend-icon="mdi-account-circle">
+      <template v-slot:activator>
+          <v-list-item-title>Users</v-list-item-title>
+        </template>
+    </v-list-group>
+    </v-navigation-drawer>
   </v-main>
 </template>
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({ drawer: false }),
 };
 </script>
