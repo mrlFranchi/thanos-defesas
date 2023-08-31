f3879ff3887c985f790b97abeee612ef3663a8ed
mRlFranchi <murilo.franchi@usp.br>
2023-08-31 20:38:37 -0300

    Made Navigation Drawer and Search box

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,59 @@
   <v-main style="padding: 0;">
   <v-toolbar>
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>Application</v-toolbar-title>
+
+    <v-spacer/>
+
+    <v-toolbar-title><v-text-field
+      label="Busca por nome"
+      v-model="nome"
+      single-line
+    /></v-toolbar-title>
+    <v-spacer/>
   </v-toolbar>
   <v-navigation-drawer
     v-model="drawer"
   >
-    <!--  -->
+  <v-card>
+    <v-list-group
+      :value="true"
+    >
+      <template v-slot:activator>
+        <v-list-item-title>Programas</v-list-item-title>
+      </template>
+        <v-radio-group
+        v-model="programa"
+        @click="console.log(programa)">
+          <v-radio
+            v-for="n in programas"
+            :key="n"
+            :label="`${n}`"
+            :value="n"
+            @click="click"
+          ></v-radio>
+        </v-radio-group>
+    </v-list-group>
+  </v-card>
+  <v-card>
+    <v-list-group
+      :value="true"
+    >
+      <template v-slot:activator>
+        <v-list-item-title>Cursos</v-list-item-title>
+      </template>
+        <v-radio-group
+        v-model="curso"
+        @click="console.log(curso)">
+          <v-radio
+            v-for="n in cursos"
+            :key="n"
+            :label="`${n}`"
+            :value="n"
+            @click="click"
+          ></v-radio>
+        </v-radio-group>
+    </v-list-group>
+  </v-card>
   </v-navigation-drawer>
     <!--  -->
   </v-main>
@@ -15,6 +62,19 @@
 
 <script>
 export default {
-  data: () => ({ drawer: null }),
+  data: () => ({
+    drawer: false,
+    nome: '',
+    programas: ['Todos', 'MAT', 'CCMC', 'PIPGEs', 'PROFMAT'],
+    programa: 'Todos',
+    cursos: ['Todos', 'Mestrado', 'Doutorado'],
+    curso: 'Todos',
+
+  }),
+  methods: {
+    click() {
+      console.log(this.programa);
+    },
+  },
 };
 </script>
