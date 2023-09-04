c8e2f5d65b2559a29044811391b164ec8f2e4062
mRlFranchi <murilo.franchi@usp.br>
2023-09-04 14:53:25 -0300

    Fixed Navigation drawer and right drawer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
+  <div>
   <v-main style="padding: 0;">
-  <v-toolbar>
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar
+    clipped-left>
+    <v-btn @click="drawer = !drawer">Filtros</v-btn>
 
     <v-spacer/>
 
@@ -11,65 +13,75 @@
       single-line
     /></v-toolbar-title>
     <v-spacer/>
-  </v-toolbar>
-  <v-navigation-drawer
-    v-model="drawer"
-  >
-  <v-card>
-    <v-list-group
-      :value="true"
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
     >
-      <template v-slot:activator>
-        <v-list-item-title>Programas</v-list-item-title>
-      </template>
-        <v-radio-group
-        v-model="programa"
-        @click="console.log(programa)">
-          <v-radio
-            v-for="n in programas"
-            :key="n"
-            :label="`${n}`"
-            :value="n"
-            @click="click"
-          ></v-radio>
-        </v-radio-group>
-    </v-list-group>
-  </v-card>
-  <v-card>
-    <v-list-group
-      :value="true"
+    <v-card>
+      <v-list-group
+        :value="true"
+      >
+        <template v-slot:activator>
+          <v-list-item-title>Programas</v-list-item-title>
+        </template>
+          <v-radio-group
+          v-model="programa">
+            <v-radio
+              v-for="n in programas"
+              :key="n"
+              :label="`${n}`"
+              :value="n"
+              @click="click"
+            ></v-radio>
+          </v-radio-group>
+      </v-list-group>
+    </v-card>
+    <v-card>
+      <v-list-group
+        :value="true"
+      >
+        <template v-slot:activator>
+          <v-list-item-title>Cursos</v-list-item-title>
+        </template>
+          <v-radio-group
+          v-model="curso">
+            <v-radio
+              v-for="n in cursos"
+              :key="n"
+              :label="`${n}`"
+              :value="n"
+              @click="click"
+            ></v-radio>
+          </v-radio-group>
+      </v-list-group>
+    </v-card>
+    </v-navigation-drawer>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      absolute
+      temporary
+      right
     >
-      <template v-slot:activator>
-        <v-list-item-title>Cursos</v-list-item-title>
-      </template>
-        <v-radio-group
-        v-model="curso"
-        @click="console.log(curso)">
-          <v-radio
-            v-for="n in cursos"
-            :key="n"
-            :label="`${n}`"
-            :value="n"
-            @click="click"
-          ></v-radio>
-        </v-radio-group>
-    </v-list-group>
-  </v-card>
-  </v-navigation-drawer>
-    <!--  -->
+      <v-card>
+        teste
+      </v-card>
+    </v-navigation-drawer>
   </v-main>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
     drawer: false,
+    rightDrawer: false,
     nome: '',
     programas: ['Todos', 'MAT', 'CCMC', 'PIPGEs', 'PROFMAT'],
     programa: 'Todos',
     cursos: ['Todos', 'Mestrado', 'Doutorado'],
     curso: 'Todos',
-
   }),
   methods: {
     click() {
